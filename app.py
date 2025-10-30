8fb57189f89e11ca9c8d2c45f3620429c97e582d
phamnhatquang112115-star <phamnhatquang112115@gmail.com>
2025-10-30 21:26:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, requfrom flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string
 import cv2
 import numpy as np
 import pytesseract
